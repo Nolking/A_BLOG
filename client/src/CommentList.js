379f61332ec05da308a20b731887fe326d6a0722
Nolan <nolan.nn235@gmail.com>
2021-05-30 14:02:57 +0700

    update api

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -1,21 +1,33 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+// import React, { useState, useEffect } from "react";
+// import axios from "axios";
 
-const CommentList = ({ postId }) => {
-  const [comments, setComments] = useState([]);
+// const CommentList = ({ postId }) => {
+//   const [comments, setComments] = useState([]);
 
-  const fetchData = async () => {
-    const res = await axios.get(
-      `http://localhost:4001/posts/${postId}/comments`
-    );
+//   const fetchData = async () => {
+//     const res = await axios.get(
+//       `http://localhost:4001/posts/${postId}/comments`
+//     );
 
-    setComments(res.data);
-  };
+//     setComments(res.data);
+//   };
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+//   useEffect(() => {
+//     fetchData();
+//   }, []);
 
+//   const renderedComments = comments.map((comment) => {
+//     return <li key={comment.id}>{comment.content}</li>;
+//   });
+
+//   return <ul>{renderedComments}</ul>;
+// };
+
+// export default CommentList;
+
+import React from "react";
+
+const CommentList = ({ comments }) => {
   const renderedComments = comments.map((comment) => {
     return <li key={comment.id}>{comment.content}</li>;
   });
